130531cacf4a75de3b22af6e9d4b91577dba8f24
Madhu Pandey <pandeymadhu80@gmail.com>
2025-04-14 23:41:38 +0530

    UPDATED PAYMENT AMOUNT FOR PRO PACKAGE

--- constants/index.ts
+++ constants/index.ts
@@ -71,7 +71,7 @@ export const navLinks = [
       _id: 2,
       name: "Pro Package",
       icon: "/assets/icons/free-plan.svg",
-      price: 40,
+      price: 50,
       credits: 120,
       inclusions: [
         {
